ca71000be87797528ed9dc897bade421ad234106
ShyamanthaPerera <shyamanthaperera64@gmail.com>
2023-12-12 15:48:43 +0530

    Constructor through Injection done

--- Constructor-Injection/src/main/java/lk/ijse/Boy.java
+++ Constructor-Injection/src/main/java/lk/ijse/Boy.java
@@ -2,13 +2,22 @@ package lk.ijse;
 
 public class Boy {
 
-    Agreement agreement = new Girl();
+    Agreement agreement;
+
+    public Boy(Agreement a){
+        this.agreement = a;
+    }
+
+    public Boy(){
+
+    }
+
     public void chatWithGirl(){
         agreement.chat();
     }
 
     public static void main(String[] args) {
-        Boy boy=new Boy();
+        Boy boy=new Boy(new Girl());
         boy.chatWithGirl();
     }
 }
