ebf817b75311ef3885acd5ea527554532f5a4b65
ShyamanthaPerera <shyamanthaperera64@gmail.com>
2023-12-12 22:28:50 +0530

    Setter method through injection done

--- Setter-Method-Injection/src/main/java/lk/ijse/Boy.java
+++ Setter-Method-Injection/src/main/java/lk/ijse/Boy.java
@@ -11,10 +11,4 @@ public class Boy {
     public void chatWithGirl(){
         agreement.chat();
     }
-
-    public static void main(String[] args) {
-        Boy boy = new Boy();
-        boy.setter(new Girl());
-        boy.chatWithGirl();
-    }
 }
---
+++ Setter-Method-Injection/src/main/java/lk/ijse/Main.java
@@ -0,0 +1,9 @@
+package lk.ijse;
+
+public class Main {
+    public static void main(String[] args) {
+        Boy boy = new Boy();
+        boy.setter(new Girl());
+        boy.chatWithGirl();
+    }
+}
